e584d1f075f0275c6177be425961a0a4a09052ce
Hehoku <hehoku@outlook.com>
2022-03-28 15:15:59 +0800

    change to eth/usd, and add comment about convert

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -12,28 +12,32 @@ contract Lottery {
   AggregatorV3Interface internal ethUsdPriceFeed;
 
   /**
+   * https://docs.chain.link/docs/matic-addresses/#Mumbai%20Testnet
    * Network: Mumbai Testnet
-   * Aggregator: MATIC/USD
-   * Address: 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
+   * Aggregator: ETH/USD
+   * Address: 0x0715A7794a1dc8e42615F059dD6e406A6594651A
    */
   constructor() public {
     usdEntryFee = 50 * (10**18);
     ethUsdPriceFeed = AggregatorV3Interface(
-      0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
+      0x0715A7794a1dc8e42615F059dD6e406A6594651A
     );
   }
 
   // @notice Register a player
   function enter() public payable {
     // $50 USD entry fee
+    require(msg.value >= getEntranceFee(), "Not enough MATIC");
     players.push(msg.sender);
   }
 
   function getEntranceFee() public view returns (uint256) {
+    // price in eth/usd with 10 ** 8
     (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
 
-    uint256 adjustedPrice = uint256(price) * 10**10;
-    uint256 costToEnter = usdEntryFee / adjustedPrice;
+    uint256 adjustedPrice = uint256(price) * 10**10; // now it'is 18 decimals
+    uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+    // return wei
     return costToEnter;
   }
 }
